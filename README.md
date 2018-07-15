98ee24c73fa17f03f918ec5aa1de8b56f6b300ac
apizzimenti <pizzimentianthony@gmail.com>
2018-07-14 23:37:49 -0400

    Add a check for the Preprocessing library.

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 This project is a wrapper for doing proration, graph analysis, and MCMC
 districting analysis for Utah.
 
+## Installation
+**If you want to do preprocessing**, navigate to this repository and run
+`$ git clone https://github.com/gerrymandr/Preprocessing.git`. Then make sure
+all the required packages are installed, and you're set.
+
+**If you don't want to do preprocessing**, then don't do anything. Skip to
+the usage section.
+
 ## Usage
 As of now, simply run `$ ./utah.sh <number of runs>` (e.g. `$./utah.sh 1000`) to do all the things
 (including proration). Then, output is stored in `./output`, including a short
--- utah.sh
+++ utah.sh
@@ -1,10 +1,16 @@
 
-# Run proration and get the report + protated data.
-echo "Starting preprocessing to generate a report."
-python3 ./Preprocessing/main.py
-mv ./Prorated.* ./Prorated/
-mv ./Proration.html ./output/Proration.html
-mv ./Proration_images ./output/Proration_images
+# Check to see that Preprocessing is installed. if so, then we run proration
+# and get the report + protated data.
+if [ -d ./Preprocessing ]
+   then
+        echo "Starting preprocessing to generate a report."
+        python3 ./Preprocessing/main.py
+        mv ./Prorated.* ./Prorated/
+        mv ./Proration.html ./output/Proration.html
+        mv ./Proration_images ./output/Proration_images
+    else
+        echo "Preprocessing library not installed - skipping."
+fi
 
 # Run the data analysis (to make sure it's all updated)
 echo "Running data analysis on the dual graph."
